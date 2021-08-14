2fe3d696769c0d0c1cbe5a0afd5b4a234605c6e1
1802802 <35828295+1802802@users.noreply.github.com>
2021-08-15 01:34:13 +0800

    Initial commit

---
+++ src/main/java/springstudy/springinaction4th/SpringInAction4thApplication.java
@@ -0,0 +1,13 @@
+package springstudy.springinaction4th;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInAction4thApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringInAction4thApplication.class, args);
+    }
+
+}
---
+++ src/test/java/springstudy/springinaction4th/SpringInAction4thApplicationTests.java
@@ -0,0 +1,13 @@
+package springstudy.springinaction4th;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInAction4thApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
